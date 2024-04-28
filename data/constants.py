8d07c6296b975a862317dfe7b1adce81047d4cd5
Ryan <ryanhasman10@hotmail.com>
2024-04-28 16:49:18 -0700

    engine constants

---
+++ data/constants.py
@@ -0,0 +1,6 @@
+# List of engine constants, not really meant for game constants.
+
+KEYINACTIVE = 0 # When input/key has no state
+KEYPRESSED = 1  # When input/key is actively being pressed
+KEYDOWN = 2     # When input/key is pressed down (for 1 frame)
+KEYUP = 3       # When input/key is released (for 1 frame)
