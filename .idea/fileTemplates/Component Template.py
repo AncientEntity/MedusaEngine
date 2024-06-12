cdfafd214291bb73dc1224092260d9bcc8b87d24
Ryan <ryanhasman10@hotmail.com>
2024-06-11 19:06:39 -0700

    file templates

---
+++ .idea/fileTemplates/Component Template.py
@@ -0,0 +1,5 @@
+from engine.ecs import Component
+
+class TemplateComponent(Component):
+    def __init__(self):
+        super().__init__()
---
+++ .idea/fileTemplates/LevelScene Template.py
@@ -0,0 +1,9 @@
+from engine.datatypes.spritesheet import SpriteSheet
+from engine.scenes.levelscene import LevelScene
+
+class TemplateLevelScene(LevelScene):
+    def __init__(self):
+        super().__init__("game/tiled/changeme.tmj", SpriteSheet("game/art/changeme.png",16), None)
+        
+        #Append systems here like this: self.systems.append()
+        #By default a LevelScene just has a rendering system.
