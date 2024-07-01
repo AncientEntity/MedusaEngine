764656e6647ae5b1333f83b6d2f2a11af6f89db6
Ryan <ryanhasman10@hotmail.com>
2024-06-30 22:55:27 -0700

    differentiated SetTint/SetColor for now.

--- game/prefabs/JammedParticlesPrefab.py
+++ game/prefabs/JammedParticlesPrefab.py
@@ -5,7 +5,7 @@ def CreateJammedParticlesPrefab(currentScene, position):
     jammedParticles = ParticleEmitterComponent()
     jammedParticles.lifeTimeRange = [0.5, 1]
     jammedParticles.sprite.SetScale((0.5, 0.5))
-    jammedParticles.sprite.SetTint((180, 10, 10))
+    jammedParticles.sprite.SetColor((180, 10, 10))
     jammedParticles.spawnBounds = pygame.Rect(-4, -4, 4, 4)
     jammedParticles.drawOrder = 50
     return currentScene.CreateEntity("Jammed Particles", position, [jammedParticles])
