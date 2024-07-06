609049ca9cfbcfc254baf188684bfc12007f970f
Ryan <ryanhasman10@hotmail.com>
2024-07-06 11:25:10 -0700

    merged engine from master into tiny-factory-remake

--- engine/systems/renderer.py
+++ engine/systems/renderer.py
@@ -1,5 +1,3 @@
-import math
-
 import pygame.display
 
 from engine.components.rendering.particlecomponent import ParticleEmitterComponent, Particle
@@ -8,7 +6,7 @@ from engine.components.rendering.spriterenderer import SpriteRenderer
 from engine.components.rendering.textrenderer import TextRenderer
 from engine.components.rendering.tilemaprenderer import TilemapRenderer
 from engine.datatypes.sprites import GetSprite, Sprite
-from engine.ecs import EntitySystem, Scene, Component
+from engine.ecs import EntitySystem, Scene
 from engine.logging import Log, LOG_ALL
 
 
@@ -71,7 +69,6 @@ class RenderingSystem(EntitySystem):
 
     def Update(self,currentScene : Scene):
         self._renderTarget.fill(self.backgroundColor)
-        self.cameraPosition = [self.cameraPosition[0],self.cameraPosition[1]]
 
         self.rawMousePosition = pygame.mouse.get_pos()
         self.screenMousePosition = ((self.rawMousePosition[0] - self._screenSize[0] / 2) / self.renderScale,(self.rawMousePosition[1] - self._screenSize[1] / 2) / self.renderScale)
@@ -194,7 +191,7 @@ class RenderingSystem(EntitySystem):
         self._renderTarget.blit(actualSprite, (renderPosition[0]-textRenderer._alignOffset[0],renderPosition[1]-textRenderer._alignOffset[1]))
 
     def WorldToScreenPosition(self,position):
-        return [position[0] - self.cameraPosition[0] + self._scaledHalfSize[0], position[1] - self.cameraPosition[1] + self._scaledHalfSize[1]]
+        return [round(position[0] - self.cameraPosition[0] + self._scaledHalfSize[0]), round(position[1] - self.cameraPosition[1] + self._scaledHalfSize[1])]
     def ScreenToWorldPosition(self, position):
         return [position[0] + self.cameraPosition[0] - self._scaledHalfSize[0], position[1] + self.cameraPosition[1] - self._scaledHalfSize[1]]
 
