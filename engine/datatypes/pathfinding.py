ce3a1576ec6fc9bbdaad1690f0ebb888f066bf56
Ryan <ryanhasman10@hotmail.com>
2024-07-13 12:33:31 -0700

    A* done

--- engine/datatypes/pathfinding.py
+++ engine/datatypes/pathfinding.py
@@ -1,4 +1,5 @@
-from engine.components.rendering.tilemaprenderer import Tilemap, TilemapRenderer
+from engine.components.rendering.tilemaprenderer import TilemapRenderer
+from engine.tools.math import Distance
 
 
 class TilemapPathfinder:
@@ -17,6 +18,87 @@ class TilemapPathfinder:
                 continue
             else:
                 isEmpty = False
-                if tileID in self.blockingPhysicsLayers:
+                if layer.physicsLayer in self.blockingPhysicsLayers:
                     return True
         return isEmpty and self.avoidEmptyTiles
+
+    def Solve(self, startIndex, endIndex):
+        nodes = self.CreateEmptyMap()
+        headIndexes = [startIndex]
+        def CalculateNodeData(nodeIndex, prevNode):
+            if(nodes[nodeIndex[0]][nodeIndex[1]] != None):
+                return
+            gValue = Distance(nodeIndex,startIndex)
+            fValue = Distance(nodeIndex,endIndex)
+            nodes[nodeIndex[0]][nodeIndex[1]] = (gValue,fValue,gValue+fValue, prevNode)
+
+        def ConstructPath():
+            curIndex = endIndex
+            path = [endIndex]
+            while(curIndex != startIndex):
+                previous = nodes[curIndex[0]][curIndex[1]][3]
+                path.insert(0,previous)
+                curIndex = previous
+            return path
+
+        def IsValidIndex(index):
+            return index[0] > 0 and index[1] > 0 and index[0] < len(nodes) and index[1] < len(nodes[0]) and not self.IsTileBlocking(index)
+
+        def GetNextValidIndexes(index):
+            surroundingIndexes = []
+
+            # Up
+            if IsValidIndex((index[0],index[1]-1)):
+                surroundingIndexes.append((index[0],index[1]-1))
+            # Down
+            if IsValidIndex((index[0],index[1]+1)):
+                surroundingIndexes.append((index[0],index[1]+1))
+            # Left
+            if IsValidIndex((index[0]-1,index[1])):
+                surroundingIndexes.append((index[0]-1,index[1]))
+            # Right
+            if IsValidIndex((index[0]+1,index[1])):
+                surroundingIndexes.append((index[0]+1,index[1]))
+
+            newIndexes = []
+            for index in surroundingIndexes:
+                if nodes[index[0]][index[1]] == None:
+                    newIndexes.append(index)
+            return newIndexes
+
+        def SortNewHeadIndex(index, prevIndex):
+            if(nodes[index[0]][index[1]] != None):
+                return
+            else:
+                CalculateNodeData(index, prevIndex)
+                i = 0
+                while i < len(headIndexes) and nodes[headIndexes[i][0]][headIndexes[i][1]][2] < nodes[index[0]][index[1]][2]:
+                    i += 1
+                headIndexes.insert(i,index)
+
+
+        CalculateNodeData(startIndex, None)
+
+        while len(headIndexes) > 0:
+            for index in headIndexes[:]:
+                if(index[0] == endIndex[0] and index[1] == endIndex[1]):
+                    return ConstructPath() # Reconstruct the path and return
+                else:
+                    nextValidIndexes = GetNextValidIndexes(index)
+                    for newIndex in nextValidIndexes:
+                        SortNewHeadIndex(newIndex,index)
+
+                headIndexes.remove(index)
+
+
+        return None # No path found
+
+
+    def CreateEmptyMap(self):
+        nodes = []
+        for x in range(len(self.tilemaps[0].tileMap.map)):
+            row = []
+            nodes.append(row)
+            for y in range(len(self.tilemaps[0].tileMap.map[0])):
+                row.append(None)
+        return nodes
--- game/systems/TestSystem.py
+++ game/systems/TestSystem.py
@@ -1,5 +1,7 @@
 from engine.components.rendering.tilemaprenderer import Tilemap, TilemapRenderer
+from engine.datatypes.pathfinding import TilemapPathfinder
 from engine.ecs import EntitySystem, Scene
+from engine.engine import Input
 from engine.scenes.levelscene import LevelScene
 from engine.systems.renderer import RenderingSystem
 
@@ -8,13 +10,41 @@ class TestSystem(EntitySystem):
     def __init__(self):
         super().__init__([]) #Put target components here
         self.hoverTileLayer : TilemapRenderer  = None
+        self.pathTileLayer : TilemapRenderer  = None
         self.renderingSystem : RenderingSystem = None
         self.lastPos = (0,0)
+
+        self.startPos = None
+        self.endPos = None
+
+        self.pathfinding : TilemapPathfinder = None
     def Update(self,currentScene : Scene):
         tilePos = self.hoverTileLayer.WorldPositionToTileIndex(self.renderingSystem.worldMousePosition)
-        self.hoverTileLayer.tileMap.SetTile(-1,self.lastPos[0],self.lastPos[1])
-        self.hoverTileLayer.tileMap.SetTile(34,tilePos[0],tilePos[1])
+        if(tilePos == self.startPos):
+            return
+        if(self.lastPos != self.startPos and self.lastPos != self.endPos):
+            self.hoverTileLayer.tileMap.SetTile(-1,self.lastPos[0],self.lastPos[1])
+            self.hoverTileLayer.tileMap.SetTile(34,tilePos[0],tilePos[1])
         self.lastPos = tilePos
+        if (Input.MouseButtonPressed(0)):
+            if(self.startPos != None):
+                self.hoverTileLayer.tileMap.SetTile(-1, self.startPos[0], self.startPos[1])
+            self.startPos = tilePos
+            self.hoverTileLayer.tileMap.SetTile(419, self.startPos[0], self.startPos[1])
+        if (Input.MouseButtonPressed(2)):
+            if(self.endPos != None):
+                self.hoverTileLayer.tileMap.SetTile(-1, self.endPos[0], self.endPos[1])
+            self.endPos = tilePos
+            self.hoverTileLayer.tileMap.SetTile(417, self.endPos[0], self.endPos[1])
+        if(self.startPos != None and self.endPos != None):
+            path = self.pathfinding.Solve(self.startPos,self.endPos)
+            self.pathTileLayer.tileMap.Clear()
+            if(path != None):
+                for index in path:
+                    self.pathTileLayer.tileMap.SetTile(418,index[0],index[1])
+
     def OnEnable(self, currentScene : LevelScene):
         self.hoverTileLayer = currentScene.tileMapLayersByName["Hover"]
+        self.pathTileLayer = currentScene.tileMapLayersByName["Path"]
         self.renderingSystem = currentScene.GetSystemByClass(RenderingSystem)
+        self.pathfinding = TilemapPathfinder(list(currentScene.tileMapLayersByName.values()), [0], True)
