fdd30eb13b070cb13e67a015a3e1a1eed45cdedd
Ryan <ryanhasman10@hotmail.com>
2024-08-10 18:04:30 -0700

    LevelScene variable type fixes and MoveTowardsDelta

--- engine/tools/math.py
+++ engine/tools/math.py
@@ -5,13 +5,19 @@ def Distance(vec1,vec2):
     b = vec1[1] - vec2[1]
     return math.sqrt(a*a+b*b)
 
-def MoveTowards(current, target, delta):
+# The given delta MoveTowards applies.
+def MoveTowardsDelta(current, target, delta):
     x = target[0]-current[0]
     y = target[1]-current[1]
     length = Distance(current,target)
     if(length <= delta or length == 0):
         return current
     normalizedVecAndDelta = (x/length*delta,y/length*delta)
+    return normalizedVecAndDelta
+
+# Gives the final position after moving towards
+def MoveTowards(current, target, delta):
+    normalizedVecAndDelta = MoveTowardsDelta(current,target,delta)
     return [current[0]+normalizedVecAndDelta[0],current[1]+normalizedVecAndDelta[1]]
 
 def NormalizeVec(vec):
