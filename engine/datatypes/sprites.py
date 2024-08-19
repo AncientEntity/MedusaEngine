934c05be6ea8e5cbb9cfbde98ee862bd7b5903ea
Ryan <ryanhasman10@hotmail.com>
2024-08-18 22:36:53 -0700

    Update sprites.py

--- engine/datatypes/sprites.py
+++ engine/datatypes/sprites.py
@@ -174,9 +174,11 @@ class AnimatedSprite(Sprite):
                 self._sprites[i] = pygame.transform.flip(self._sprites[i], True, False)
             else:
                 self._sprites[i].SetFlipX(flipped)
+        return self
     def SetTint(self,tint):
         for sprite in self._sprites:
             sprite.SetTint(tint)
+        self._tint = tint
     def SetRotation(self,rotation):
         for sprite in self._sprites:
             sprite.SetRotation(rotation)
