50fb9551d1be058ec5f2af2a369c19262306eeea
Ryan <ryanhasman10@hotmail.com>
2024-10-10 00:34:21 -0700

    better color lights

--- engine/systems/lighting.py
+++ engine/systems/lighting.py
@@ -1,4 +1,4 @@
-from pygame import BLEND_RGBA_SUB, BLEND_RGB_ADD
+from pygame import BLEND_RGBA_SUB, BLEND_RGB_ADD, BLEND_RGBA_ADD
 
 from engine.components.rendering.lightcomponent import LightComponent
 from engine.components.rendering.spriterenderer import SpriteRenderer
@@ -6,10 +6,11 @@ from engine.datatypes.sprites import Sprite
 from engine.ecs import EntitySystem, Scene, Component, Entity
 import pygame
 
+from engine.engine import Input
 from engine.logging import Log, LOG_ERRORS, LOG_INFO
 from engine.scenes.levelscene import LevelScene
 from engine.systems.renderer import RenderingSystem
-from engine.tools.math import Clamp
+from engine.tools.math import Clamp, Magnitude
 
 
 class LightingSystem(EntitySystem):
@@ -29,6 +30,17 @@ class LightingSystem(EntitySystem):
     def Update(self,currentScene : Scene):
 
         rawSurface : pygame.Surface = self._worldLightSprite.GetSprite()
+
+        # todo remove eventually
+        #if Input.KeyDown(pygame.K_l):
+        #    if self._worldLightSprite.GetSprite():
+        #        self._worldLightSprite.sprite = None
+        #    else:
+        #        lightSurface = pygame.Surface(self._rendering._scaledScreenSize, pygame.SRCALPHA, 32)
+        #        lightSurface.convert_alpha()
+        #        self._worldLightSprite.sprite = lightSurface
+        #if not rawSurface:
+        #    return
 
         # Reset the light sprite to being fully black with the world brightness alpha.
         rawSurface.fill((0, 0, 0, self.worldBrightness))
@@ -51,9 +63,9 @@ class LightingSystem(EntitySystem):
                                                        surfacePosition[1] - light.radius),
                             special_flags=BLEND_RGBA_SUB)
             # Notice this is the BLEND_RGB_ADD not BLEND_RGBA_ADD flag...
-            rawSurface.blit(light.cachedLightSurface, (surfacePosition[0] - light.radius,
+            rawSurface.blit(light.cachedColorSurface, (surfacePosition[0] - light.radius,
                                                        surfacePosition[1] - light.radius),
-                            special_flags=BLEND_RGB_ADD)
+                            special_flags=BLEND_RGBA_ADD)
 
 
     def OnEnable(self, currentScene : Scene):
@@ -83,17 +95,26 @@ class LightingSystem(EntitySystem):
     def CreateLightSprite(self, light : LightComponent):
         lightSurface = pygame.Surface((light.radius*2,light.radius*2), pygame.SRCALPHA, 32)
         lightSurface.convert_alpha()
+        colorSurface = pygame.Surface((light.radius*2,light.radius*2), pygame.SRCALPHA, 32)
+        colorSurface.convert_alpha()
 
         for i in range(light.radius):
             inverse = Clamp((1.0 - (i / light.radius)) * light.brightness,0,1)
             brightness = 255 * inverse
-            color = (light.color[0] * inverse,
+            lightColor = (light.color[0] * inverse,
                      light.color[1] * inverse,
                      light.color[2] * inverse,
                      Clamp(brightness,0 , 255))
-            pygame.draw.circle(lightSurface, color, (light.radius,light.radius), i, width=2)
+            tintColor = (light.color[0] * inverse,
+                     light.color[1] * inverse,
+                     light.color[2] * inverse,
+                     # 441.67295593006371 is the max possible magnitude (Magnitude((255,255,255))
+                     Clamp(Magnitude(light.color) / 441.67295593006371 * 255 * inverse,0 , 255))
+            pygame.draw.circle(lightSurface, lightColor, (light.radius,light.radius), i, width=2)
+            pygame.draw.circle(colorSurface, tintColor, (light.radius,light.radius), i, width=2)
 
         light.cachedLightSurface = lightSurface
+        light.cachedColorSurface = colorSurface
         light.cachedBrightness = light.brightness
         light.cachedRadius = light.radius
         light.cachedColor = light.color
