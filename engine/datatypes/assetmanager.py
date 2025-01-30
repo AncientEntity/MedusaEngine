c4fe41bb7dd27810e911e18419646ddda35dfa85
Ryan <ryanhasman10@hotmail.com>
2025-01-29 22:43:35 -0800

    beginning of asset manager

--- engine/datatypes/assetmanager.py
+++ engine/datatypes/assetmanager.py
@@ -3,3 +3,24 @@
 # to an asset here, that gets grabbed whenever it needs to be rendered.
 # The current implementation just has things like datatypes.sprites.Sprite holding the surface itself.
 # A asset manager instance should be created on start and loaded into the engine instance.
+from engine.ecs import Scene
+
+
+class AssetManager:
+    def __init__(self):
+        self.prefabs = {}
+
+
+    def Instantiate(self, prefab_name : str, currentScene : Scene):
+        return self.prefabs[prefab_name](currentScene)
+    def FactoryInstantiate(self, prefab_name : str):
+        def _Instantiate(_currentScene : Scene):
+            return self.Instantiate(prefab_name, _currentScene)
+        return _Instantiate
+
+
+
+
+
+
+assets = AssetManager()
