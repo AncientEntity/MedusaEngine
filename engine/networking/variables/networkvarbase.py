624859c77a166964d21eee17bcc4e0fa0da502e4
Ryan <ryanhasman10@hotmail.com>
2025-02-05 15:35:24 -0800

    networkvarbase hooks

--- engine/networking/variables/networkvarbase.py
+++ engine/networking/variables/networkvarbase.py
@@ -6,12 +6,16 @@ class NetworkVarBase:
         self._modified = False
         self.hasAuthority = False
 
+        self._hooks = [] # func(self) passes in self
+
 
     # Get/Set/Add as intended values
     def Set(self, value, modified=True):
         self._modified = modified
+        self.TriggerHooks()
     def Add(self, value, modified=True):
         self._modified = modified
+        self.TriggerHooks()
     def Get(self):
         pass
 
@@ -21,3 +25,9 @@ class NetworkVarBase:
         self._modified = modified
     def GetAsBytes(self):
         pass
+
+    def AddHook(self, func):
+        self._hooks.append(func)
+    def TriggerHooks(self):
+        for func in self._hooks:
+            func(self)
