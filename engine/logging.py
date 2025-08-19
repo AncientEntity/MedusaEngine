f30f2612a6f88d4a4eb405c91ea3a9cd8946a032
Ryan <rogoclub@outlook.com>
2025-08-19 14:00:00 -0700

    Update logging.py

--- engine/logging.py
+++ engine/logging.py
@@ -18,7 +18,7 @@ LOG_NETWORKING = 2.1
 LOG_NETWORKPROCESS = 3.1
 
 # Log Settings
-LOG_LEVEL = LOG_INFO
+LOG_LEVEL = LOG_INFO # todo load this and other logging settings from a cfg file.
 MAX_LOG_FILES = 10
 
 # Controls how often Log() will flush the log file's buffer.
